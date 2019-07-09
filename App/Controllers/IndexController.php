6c9ce9079eb362283bc910b82978e9c020126bba
Israel Hudson <israelhudson@gmail.com>
2019-07-09 11:11:20 -0300

    definindo rotas e controllers

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Controllers;
+
+class IndexController
+{
+    public function index()
+    {
+        echo "Route: / Controlller: index";
+    }
+
+    public function contact()
+    {
+        echo "Route: /contact Controlller: contact";
+    }
+}
